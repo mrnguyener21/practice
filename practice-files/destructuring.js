67005a11e334d0bc34931aac104b402ee96afd8f
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-03-31 20:40:06 -0700

    example 5

--- practice-files/destructuring.js
+++ practice-files/destructuring.js
@@ -8,8 +8,8 @@ const array = [
     {a:1,b:2,c:3},
     {a:3,b:4,c:5},
 ]
-const {a,b,c} = array[0];
-console.log(a,b,c);//1,2,3;
+const {a,b,c} = array[1];
+console.log(a,b,c);//4,5,6;
 
 //example 3
 const arrayWithObjects = [
@@ -20,4 +20,26 @@ const objects = arrayWithObjects.map((object)=>{
    const{a,b,c} = object;
    return ({a,b,c});
 })
-console.log(objects)
+console.log(objects)// [{1,2,3},{4,5,6}]
+
+//example 4
+const arrayWithObjects = [
+    {a:1,b:2,c:3},
+    {a:3,b:4,c:5},
+]
+const objects = arrayWithObjects.map((object)=>{
+   const{a,b,c} = object;
+   return (a,b,c);
+})
+console.log(objects)//[3,5] why is it only returning c
+
+//example 5
+const arrayWithObjects = [
+    {a:1,b:2,c:3},
+    {a:3,b:4,c:5},
+]
+const objects = arrayWithObjects.map((object)=>{
+   const{a,b,c} = object;
+   return (object);
+})
+console.log(objects) //[{1,2,3},{4,5,6}], this is the case, because you are ignoring the assigning to const a, b & c and returning the actually object parameter from each iteration
