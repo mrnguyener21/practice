bda9b8476a1f8c346ea4cbf54a1a9d03b615d318
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-03-31 21:05:37 -0700

    example 7

--- practice-files/destructuring.js
+++ practice-files/destructuring.js
@@ -29,11 +29,22 @@ const arrayWithObjects = [
 ]
 const objects = arrayWithObjects.map((object)=>{
    const{a,b,c} = object;
+   return (a);
+})
+console.log(objects)
+
+//example 5
+const arrayWithObjects = [
+    {a:1,b:2,c:3},
+    {a:3,b:4,c:5},
+]
+const objects = arrayWithObjects.map((object)=>{
+   const{a,b,c} = object;
    return (a,b,c);
 })
 console.log(objects)//[3,5] why is it only returning c
 
-//example 5
+//example 6
 const arrayWithObjects = [
     {a:1,b:2,c:3},
     {a:3,b:4,c:5},
@@ -44,4 +55,22 @@ const objects = arrayWithObjects.map((object)=>{
 })
 console.log(objects) //[{1,2,3},{4,5,6}], this is the case, because you are ignoring the assigning to const a, b & c and returning the actually object parameter from each iteration
 
+//example 7 USING EXAMPLE 2 FROM FETCHING.JS
+import axios from 'axios';
+
+const URL = 'enter database url here';
+
+const fetchColors = async () => {
+    try{
+        const response = await axios.get(URL);
+        const colors = response.data.map((color)=> {
+            const{warm, cold, primary, secondary} = color
+            return ({warm, cold, primary, secondary});//should return an array with each index being an object containing data that matches the property name of warm, cold, primary and secondary
+        })
+        return colors;
+    }catch(error){
+        console.log('NO DOGGIES FOUND')
+    }
+}
+export default fetchColors;
 
